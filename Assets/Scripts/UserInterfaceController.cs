5e8967d04fa0dd020b70250cb361dbc60f41b0a4
PranavAlla <Pranavalla@gmail.com>
2025-09-16 13:56:34 -0400

    Added basic functionality to main menu, pause menu, and settings UI and made UI persist between scenes

--- Assets/Scripts/UserInterfaceController.cs
+++ Assets/Scripts/UserInterfaceController.cs
@@ -1,32 +1,112 @@
+using System.Collections;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UserInterfaceController : MonoBehaviour
 {
+    [SerializeField] private GameObject MainMenuCanvas;
+    [SerializeField] private GameObject PauseMenuCanvas;
+    [SerializeField] private GameObject SettingsMenuCanvas;
+
+    private bool isPaused = false;
+
     private enum UIState { NONE, MENU, PAUSE }
     private UIState uiState = UIState.NONE;
 
+    private static UserInterfaceController UserInterface;
+
+    void Awake()
+    {
+        // Prevention of duplicate UI gameObjects and makes UI persist between scenes
+        if (UserInterface == null)
+        {
+            UserInterface = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
     void Start()
     {
-
+        uiState = UIState.MENU; // Temporary line for testing menu in programming test scene
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.P) && uiState != UIState.MENU)
+        {
+            if (isPaused) Unpause();
+            else if (!isPaused) Pause();
+        }
+    }
 
+    public void Pause()
+    {
+        isPaused = true;
+        Time.timeScale = 0;
+        PauseMenuCanvas.SetActive(true);
+    }
+
+    public void Unpause()
+    {
+        isPaused = false;
+        Time.timeScale = 1.0f;
+        PauseMenuCanvas.SetActive(false);
     }
 
     public void OnPlayPressed()
     {
-        print("Play button pressed");
+        SceneManager.LoadScene("ProgrammingTestScene");
+        MainMenuCanvas.SetActive(false);
+        uiState = UIState.PAUSE;
     }
 
     public void OnSettingsPressed()
     {
-        print("Settings pressed");
+        switch (uiState)
+        {
+            case UIState.MENU:
+                MainMenuCanvas.SetActive(false);
+                SettingsMenuCanvas.SetActive(true);
+                break;
+            case UIState.PAUSE:
+                PauseMenuCanvas.SetActive(false);
+                SettingsMenuCanvas.SetActive(true);
+                break;
+
+        }
     }
 
     public void OnQuitPressed()
     {
         print("Quit button pressed");
+    }
+
+    public void OnBackPressed()
+    {
+        switch (uiState)
+        {
+            case UIState.MENU:
+                MainMenuCanvas.SetActive(true);
+                SettingsMenuCanvas.SetActive(false);
+                break;
+            case UIState.PAUSE:
+                PauseMenuCanvas.SetActive(true);
+                SettingsMenuCanvas.SetActive(false);
+                break;
+        }
+    }
+
+    public void OnQuitToMenuPressed()
+    {
+        SceneManager.LoadScene("ProgrammingTestScene"); // Currently just loads programming test scene in future should change to menu scene
+        MainMenuCanvas.SetActive(true);
+        PauseMenuCanvas.SetActive(false);
+        uiState = UIState.MENU;
     }
 }
