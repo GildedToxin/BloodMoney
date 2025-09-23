1ae125ff24603eb5b74b6347ff164156d666bba1
Toxin4426 <Toxin4426@gmail.com>
2025-09-23 16:05:44 -0400

    clean up PC

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,25 +2,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int money = 20;
     public InventoryUIManager inventoryUIManager;
-    public InventoryController inventoryController;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.E))
         {
             inventoryUIManager.gameObject.SetActive(!inventoryUIManager.gameObject.activeSelf);
-        }
-        if(Input.GetKeyDown(KeyCode.R))
-        {
-            inventoryController.BuyItem(Resources.Load<Item>("Items/DebugItem"), 10, this);
         }
     }
 }
