a38b52988afbbbbb5ac16fe474465774dc428d6c
Necraza <105610460+Necraza@users.noreply.github.com>
2025-09-22 22:40:08 -0400

    Player Jump
    A player jump

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,6 +5,21 @@ public class PlayerController : MonoBehaviour
     [Header("Movement")]
     public float moveSpeed;
 
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    private bool canJump;
+
+    [Header("Keybinds")]
+     public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    public float playerHight;
+    public LayerMask Ground;
+    private bool grounded;
+
     public Transform orientation;
 
     private float horizontalInput;
@@ -18,11 +33,27 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        canJump = true;
     }
 
     private void Update()
     {
+        //grounded check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHight * 0.5f + 0.2f, Ground);
+
         MyInput();
+        SpeedControl();
+
+        // player movement drag
+        if(grounded)
+        {
+            // LinearDamping is the new term for Drag
+            rb.linearDamping = groundDrag;
+        } 
+        else
+        {
+            rb.linearDamping = 0;
+        }
     }
 
     private void FixedUpdate()
@@ -34,6 +65,16 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        //when player jumps
+        if(Input.GetKey(jumpKey) && canJump && grounded)
+        {
+            canJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown); // Calls reset jump with a delay of jumpCooldown so holding down the key keeps the player jumping
+        }
     }
 
     private void MovePlayer()
@@ -41,6 +82,41 @@ public class PlayerController : MonoBehaviour
         //finds the movement direction
         movementDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        rb.AddForce(movementDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        // on ground
+        if (grounded)
+        {
+            rb.AddForce(movementDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+
+        // in air
+        else if (!grounded)
+        {
+            rb.AddForce(movementDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        // limit velocity as needed
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        //Reset Y velocity
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        canJump = true;
     }
 }
