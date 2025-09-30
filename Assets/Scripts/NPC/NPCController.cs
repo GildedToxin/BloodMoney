8e21b8bab602f0fdfa14243b9a125eeea03d3380
PranavAlla <Pranavalla@gmail.com>
2025-09-30 16:42:26 -0400

    Saving work on NPC: - Created a test whitebox prefab with navmesh surface - Created an NPC with NPCController script - Added package for nav mesh surface

---
+++ Assets/Scripts/NPC/NPCController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using System.Collections.Generic;
+
+public class NPCController : MonoBehaviour
+{
+    [SerializeField] private Vector3 target;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
