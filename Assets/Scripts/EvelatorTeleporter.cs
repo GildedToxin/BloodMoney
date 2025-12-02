19f083a4cc911623e546531ad4483e1798a978ae
PranavAlla <Pranavalla@gmail.com>
2025-12-02 15:11:00 -0500

    Saving work on elevator

--- Assets/Scripts/EvelatorTeleporter.cs
+++ Assets/Scripts/EvelatorTeleporter.cs
@@ -1,31 +1,59 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class EvelatorTeleporter : MonoBehaviour
 {
+    public Transform currentBox;
     public Transform box1;
     public Transform box2;
+    public Transform box3;
+    public Collider collider;
 
-    private bool inBox1 = true;
+    public List<GameObject> listOfObjects = new List<GameObject>();
+
+    //private bool inBox1 = true;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.J))
         {
-
-                Teleport(box1, box2);
+            foreach (GameObject obj in listOfObjects)
+            {
+                try
+                {
+                    Teleport(currentBox, box1, obj);
+                }
+                catch
+                {
+                    Debug.Log("Teleport to box1 failed");;
+                }
+                
+            }
      
         }
+        Debug.Log(listOfObjects.Count);
     }
 
-    void Teleport(Transform fromBox, Transform toBox)
+    void Teleport(Transform fromBox, Transform toBox, GameObject obj)
     {
         // Get the player's position relative to the current box
-        Vector3 localPos = fromBox.InverseTransformPoint(transform.position);
+        Vector3 localPos = fromBox.InverseTransformPoint(obj.transform.position);
 
         // Convert that local position to world space in the new box
         Vector3 newWorldPos = toBox.TransformPoint(localPos);
 
         // Move player
-        transform.position = newWorldPos;
+        obj.transform.position = newWorldPos;
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        //compare tag
+        if (other.gameObject.tag == "Teleportable")
+            listOfObjects.Add(other.gameObject);
+    }
+    void OnTriggerExit(Collider other)
+    {
+        listOfObjects.Remove(other.gameObject);
     }
 }
