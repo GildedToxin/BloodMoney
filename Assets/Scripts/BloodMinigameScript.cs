a0f4fca5cc01985b0d2ddc233d19072a9799aacf
PranavAlla <Pranavalla@gmail.com>
2026-01-28 16:32:03 -0500

    Saving work for Blood Minigame, created a simple UI for spawning blood drops for the Rhythym game

--- Assets/Scripts/BloodMinigameScript.cs
+++ Assets/Scripts/BloodMinigameScript.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class BloodMinigameScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject bloodDropStartPoint;
+    public GameObject playerHitZone;
+    public GameObject bloodDropPrefab;
+    private float bloodDropTimer = 0.0f;
+    private float bloodDropTimeLimit = 3.0f;
+    public bool startGame = false;
+
+    private float bloodDropSpeed = 200.0f;
+
+    private List<GameObject> activeBloodDrops = new List<GameObject>();
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (startGame)
+        {
+            bloodDropTimer += Time.deltaTime;
+            if (bloodDropTimer >= bloodDropTimeLimit)
+                SpawnBloodDrop();
+            MoveBloodDrops();
+        }
+    }
+
+    private void SpawnBloodDrop()
+    {
+        GameObject newBloodDrop = Instantiate(bloodDropPrefab, new Vector3(bloodDropStartPoint.transform.position.x, bloodDropStartPoint.transform.position.y, 0), Quaternion.identity);
+        activeBloodDrops.Add(newBloodDrop);
+        newBloodDrop.GetComponent<RectTransform>().SetParent(this.transform);
+        bloodDropTimer = 0.0f;
+    }
+
+    private void MoveBloodDrops()
+    {
+        foreach (GameObject bloodDrop in activeBloodDrops)
+        {
+            bloodDrop.GetComponent<RectTransform>().position += new Vector3(-bloodDropSpeed * Time.deltaTime, 0, 0);
+
+            if (bloodDrop.transform.position.y <= playerHitZone.transform.position.y)
+            {
+                    // Handle hit logic here
+                    
+            }
+        }
     }
 }
