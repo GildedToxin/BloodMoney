a9b3ce75f2e6e19c7b399cf843b4fa2513df57ef
PranavAlla <Pranavalla@gmail.com>
2026-02-01 17:19:56 -0500

    Cleaned up code a bit and finishing touches

--- Assets/Scripts/BloodMinigameScript.cs
+++ Assets/Scripts/BloodMinigameScript.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class BloodMinigameScript : MonoBehaviour
 {
@@ -8,34 +9,33 @@ public class BloodMinigameScript : MonoBehaviour
     public GameObject bloodDropEndPoint;
     public GameObject playerHitZone;
     public GameObject bloodDropPrefab;
-    public GameObject scoreVisual;  // Displays score with a slider (should be replaced with proper visuals later)
+    public Slider scoreVisual;  // Displays score with a slider (should be replaced with proper visuals later)
 
  
     private float bloodDropTimer = 0.0f;   // Timer for spawning blood drops
-    private float bloodDropTimeLimit = 3.0f;
+    private float bloodDropTimeLimit;   // Adjust the LimitMin and Max to change spawn rate
+    private float bloodDropTimeLimitMin = 0.5f; // Minimum time limit for spawning blood drops
+    private float bloodDropTimeLimitMax = 2.0f; // Maximum time limit for spawning blood drops
 
     private bool gameRunning = false;
-    private float bloodDropSpeed = 200.0f;
-    public float minigameTime = 40f;  // Total time before minigame ends
+    private float bloodDropSpeed;
+    private float bloodDropStartSpeed = 200.0f; // Starting speed for blood drops
+    private float bloodDropMaxSpeed = 1000.0f; // Maximum speed for blood drops
+    private float bloodSpeedIncreaseRate = 50.0f; // The rate at which blood drop speed increases over time
+    public float minigameTime;  // Total time before minigame ends
+    public float minigameStartTime = 40f;  // Change this to set the minigameTime start value for the beginning of the minigame
 
     // Blood drop spawn limits
-    private int maxBloodDrops = 15; 
-    private int minBloodDrops = 10; 
-    private int currentBloodDrops = 0; // How many blood drops are left to spawn
-    private int collectedBloodDrops = 0; // How many blood drops the player has collected
-    private int totalBloodDrops = 0;  // Total blood drops to be spawned in the minigame
-    private int score = 0;
+    private float collectedBloodDrops = 0; // How many blood drops the player has collected
+    private float totalBloodDrops = 10;  // Total blood drops the player needs to collect to get a max score
+    public float score;
     public bool startGameBool = false; // make true in inspector to test minigame
 
     private List<GameObject> activeBloodDrops = new List<GameObject>();
-    void Start()
-    {
-        
-    }
 
     void Update()
     {
-        if (gameRunning && minigameTime > 0)
+        if (gameRunning && minigameTime > 0)  // Main game loop
         {
             bloodDropTimer += Time.deltaTime;
             if (bloodDropTimer >= bloodDropTimeLimit)
@@ -45,7 +45,7 @@ public class BloodMinigameScript : MonoBehaviour
 
             minigameTime -= Time.deltaTime;
             score = collectedBloodDrops / totalBloodDrops * 100;
-            scoreVisual.GetComponent<UnityEngine.UI.Slider>().value = score;
+            scoreVisual.value = score;
             if (gameRunning && Input.GetKeyDown(KeyCode.E))
             {
                 CollectBloodDrop();
@@ -56,13 +56,21 @@ public class BloodMinigameScript : MonoBehaviour
             gameRunning = false;
         }
 
+        // Stuff that should only happen once at the start of the minigame
         if (startGameBool)
         {
             StartGame();
+            score = 0;
         }
 
-        if (gameRunning && minigameTime <= 0)
+        // Stuff that should only happen once at the end of the minigame
+        if (gameRunning && minigameTime <= 0 || collectedBloodDrops >= totalBloodDrops)
         {
+            if (collectedBloodDrops >= totalBloodDrops)
+            {
+                score = 100;
+                scoreVisual.value = score;
+            }
             EndGame();
         }
     }
@@ -72,8 +80,10 @@ public class BloodMinigameScript : MonoBehaviour
         GameObject newBloodDrop = Instantiate(bloodDropPrefab, new Vector3(bloodDropStartPoint.transform.position.x, bloodDropStartPoint.transform.position.y, 0), Quaternion.identity);
         activeBloodDrops.Add(newBloodDrop);
         newBloodDrop.GetComponent<RectTransform>().SetParent(this.transform);
-        bloodDropTimer = 0.0f;
-        currentBloodDrops--;
+        bloodDropTimer = 0.0f; // Reset timer
+        bloodDropTimeLimit = Random.Range(bloodDropTimeLimitMin, bloodDropTimeLimitMax); // Randomize next spawn time
+        if (bloodDropSpeed > 600.0f)
+            bloodDropTimeLimit /= 2.0f; // Spawn blood drops faster at higher speeds
     }
 
     private void MoveBloodDrops()
@@ -90,21 +100,21 @@ public class BloodMinigameScript : MonoBehaviour
             {
                 Destroy(bloodDrop);
                 activeBloodDrops.RemoveAt(i);
+                if (collectedBloodDrops > 0)
+                    collectedBloodDrops--;
             }
         }
     }
 
     private void SpeedUpBloodDrops()
     {
-        if (bloodDropSpeed < 1000.0f)
-            bloodDropSpeed += Time.deltaTime * 50.0f;
+        if (bloodDropSpeed < bloodDropMaxSpeed)
+            bloodDropSpeed += Time.deltaTime * bloodSpeedIncreaseRate;
     }
 
     private void StartGame()
     {
         gameRunning = true;
-        currentBloodDrops = Random.Range(minBloodDrops, maxBloodDrops + 1);
-        totalBloodDrops = currentBloodDrops;
 
         // Clear any existing blood drops
         for (int i = activeBloodDrops.Count - 1; i >= 0; i--)
@@ -116,11 +126,11 @@ public class BloodMinigameScript : MonoBehaviour
         activeBloodDrops.Clear();
 
         // Reset Values
-        bloodDropSpeed = 200.0f;
+        bloodDropSpeed = bloodDropStartSpeed;
         collectedBloodDrops = 0;
-        minigameTime = 30f;
+        minigameTime = minigameStartTime;
         startGameBool = false;
-        score = 0;
+        
     }
 
     private void EndGame()
@@ -138,14 +148,20 @@ public class BloodMinigameScript : MonoBehaviour
             GameObject bloodDrop = activeBloodDrops[i];
             float minDistance = 50f;
 
-            if (Vector3.Distance(bloodDrop.GetComponent<RectTransform>().position, playerHitZone.GetComponent<RectTransform>().position) < minDistance)
+            // Checks for the closest blood drop in range
+            if (Vector3.Distance(bloodDrop.GetComponent<RectTransform>().position, 
+            playerHitZone.GetComponent<RectTransform>().position) < minDistance)
             {
                 closestBloodDrop = bloodDrop;
                 closestBloodDropIndex = i;
             }
         }
-        if (closestBloodDropIndex == -1)
+        if (closestBloodDropIndex == -1) // When there is no blood drop close enough to collect
+        {
+            if (collectedBloodDrops > 0)  // Reduces the score if the player miss times their hit
+                collectedBloodDrops--;
             return;
+        }
         Destroy(closestBloodDrop);
         activeBloodDrops.RemoveAt(closestBloodDropIndex);
         collectedBloodDrops++;
