2d20a2115578d355382abdc720ac47f03961e97b
Necraza <105610460+Necraza@users.noreply.github.com>
2026-02-17 18:42:27 -0500

    Slider start

--- Assets/Scripts/EyeMinigame/EyeMinigameController.cs
+++ Assets/Scripts/EyeMinigame/EyeMinigameController.cs
@@ -12,41 +12,44 @@ public class EyeMinigameController : MonoBehaviour
     public Slider scoopSliderHorizontal;
     private float scoopVerticalPosition = 0f;
     private float scoopHorizontalPosition = 0f;
+    public float speed = 0.01f;
+    private float SliderMovement = 0f;
 
     public void Awake()
     {
         scoopSliderVertical.value = 0;
-        scoopSliderHorizontal.value = 1;
+        scoopSliderHorizontal.value = 0;
         scoopSliderHorizontal.enabled = false;
+        SliderMovement = speed * Time.deltaTime;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         if (minigameRunning)
         {
-            if (scoopSliderVertical.enabled && !scoopSliderHorizontal.enabled)
-            {
-                Debug.Log("running");
-                if (scoopSliderVertical.value > 0)
-                {
-                    scoopSliderVertical.value += 0.1f - Time.deltaTime;
-                }
-                else if (scoopSliderVertical.value < 0)
-                {
-                    scoopSliderVertical.value -= 0.1f - Time.deltaTime;
-                }
-            }
-            else if (scoopSliderHorizontal.enabled && !scoopSliderVertical.enabled)
-            {
-                if (scoopSliderHorizontal.value > 0)
-                {
-                    scoopSliderHorizontal.value += 0.1f - Time.deltaTime;
-                }
-                else if (scoopSliderHorizontal.value < 0)
-                {
-                    scoopSliderHorizontal.value -= 0.1f - Time.deltaTime;
-                }
-            }
+            VerticalMovement();
+        }
+    }
+
+    void VerticalMovement()
+    {
+        bool fill = true;
+        if (fill)
+        {
+            scoopSliderVertical.value = scoopSliderVertical.value + SliderMovement;
+        }
+        else if (!fill)
+        {
+            scoopSliderVertical.value = scoopSliderVertical.value - SliderMovement;
+        }
+
+        if (scoopSliderVertical.value == 1)
+        {
+            fill = false;
+        }
+        else if (scoopSliderVertical.value == 0)
+        {
+            fill = true;
         }
     }
 }
