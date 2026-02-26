e1808e6da33a82a63b2f472947a679454ee7c16f
Necraza <doriangyanez@gmail.com>
2026-02-25 22:02:21 -0500

    spoon shimmy animation
    the spoon now shakes. Its spooked at your actions. How did you manage to scare a spoon? it doesn't have a brain or emotions.

--- Assets/Scripts/EyeMinigame/mashingMinigame.cs
+++ Assets/Scripts/EyeMinigame/mashingMinigame.cs
@@ -5,17 +5,22 @@ using UnityEngine.UI;
 public class mashingMinigame : MonoBehaviour
 {
     public bool isMashing = false;
+    public EyeMinigameController emc;
 
-    public Slider mashBar;
+    [Header("inputs")]
     public KeyCode button0 = KeyCode.M;
     public KeyCode button1 = KeyCode.N;
-    public int mashDuration = 100;
     private int buttonPress = 0;
+
+    [Header("duration")]
+    public Slider mashBar;
+    public int mashDuration = 100;
+
+    [Header("animation")]
     public GameObject scoop;
     public int tiltAngel1;
     public int tiltAngel2;
     public int smooth;
-    public EyeMinigameController emc;
 
     public void Start()
     {
@@ -26,28 +31,28 @@ public class mashingMinigame : MonoBehaviour
     {
         if (isMashing)
         {
-            if (Input.GetKeyDown(button0) && buttonPress == 0)
+            if (buttonPress == 0)
             {
-                mashBar.value++;
-                buttonPress = 1;
-            }
-            else if (Input.GetKeyDown(button1) && buttonPress == 1)
-            {
-                mashBar.value++;
-                buttonPress = 0;
+                UnityEngine.Quaternion target = UnityEngine.Quaternion.Euler(0, 0, tiltAngel1);
+                scoop.transform.rotation = UnityEngine.Quaternion.Slerp(scoop.transform.rotation, target, Time.deltaTime * smooth);
+
+                if (Input.GetKeyDown(button0))
+                {
+                    mashBar.value++;
+                    buttonPress = 1;
+                }
             }
 
-            if(buttonPress == 0)
-            {
-                float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngel1;
-                UnityEngine.Quaternion target = UnityEngine.Quaternion.Euler(0, 0, tiltAroundZ);
-                scoop.transform.rotation = UnityEngine.Quaternion.Slerp(scoop.transform.rotation, target, Time.deltaTime * smooth);
-            }
             else if (buttonPress == 1)
             {
-                float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngel2;
-                UnityEngine.Quaternion target = UnityEngine.Quaternion.Euler(0, 0, tiltAroundZ);
+                UnityEngine.Quaternion target = UnityEngine.Quaternion.Euler(0, 0, tiltAngel2);
                 scoop.transform.rotation = UnityEngine.Quaternion.Slerp(scoop.transform.rotation, target, Time.deltaTime * smooth);
+
+                if (Input.GetKeyDown(button1))
+                {
+                    mashBar.value++;
+                    buttonPress = 0;
+                }
             }
 
             if(mashBar.value == mashBar.maxValue)
