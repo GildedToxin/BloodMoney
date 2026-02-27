f4436db244f0f8f9ee115c6e66ba98ba563156fc
PranavAlla <Pranavalla@gmail.com>
2026-02-26 23:25:44 -0500

    Scoring and complete minigame loop

--- Assets/Scripts/BoneCuttingMiniGameScript.cs
+++ Assets/Scripts/BoneCuttingMiniGameScript.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,17 +11,25 @@ public class BoneCuttingMiniGameScript : MonoBehaviour
     [SerializeField] private Slider scoreVisual; // Whitebox slider for progression visual
     [SerializeField] private BoneMiniGameScript mainScript; // Reference to the main script for the mini game
     [SerializeField] public bool startGame = false;
+    private bool endGame = false;
 
     private Vector3 initialPlayerPos;
+    private Vector3 initialTargetPos;
+    private Vector3 targetNewPos;
     private float playerTargetRange = 25f; // how far the player must be from the target to gain points
     private bool playerInput = false;
     [SerializeField] private float score = 0; // current score
     [SerializeField] private float scoringRate = 20f; // the speed that the score goes up when the player is in range
     private float maxScore = 100f;
+    private float startTime = 60f;  // Set this to set the time limit of the cutting minigame
+    public float timer = 0f;
 
     void Start()
     {
         initialPlayerPos = player.GetComponent<RectTransform>().position;
+        initialTargetPos = targetArea.GetComponent<RectTransform>().position;
+        targetNewPos = initialTargetPos;
+        timer = startTime;
     }
 
     void Update()
@@ -39,13 +48,23 @@ public class BoneCuttingMiniGameScript : MonoBehaviour
             PlayerInput();
             GatheringPoints();
 
+            // Target area movement
+            MoveTarget();
+
             if (score < maxScore) scoreVisual.value = score; // Using slider for whiteboxing the score progress
-            if (score >= maxScore) 
+
+            // End game conditions
+            if (score >= maxScore) // winning
             {
-                mainScript.currentBone++;
-                startGame = false;
-                ResetValues();
+                EndGame();
             }
+            }
+            if (startGame) // Timer
+                timer -= Time.deltaTime;
+            if (timer <= 0) // losing (due to running out of time)
+            {
+                mainScript.failedCutting = true;
+                EndGame();
             }
     }
 
@@ -82,5 +101,31 @@ public class BoneCuttingMiniGameScript : MonoBehaviour
     {
         score = 0;
         player.GetComponent<RectTransform>().position = initialPlayerPos;
+        targetArea.GetComponent<RectTransform>().position = initialTargetPos;
+        timer = startTime;
+    }
+
+    private void MoveTarget()
+    {
+        // Selects new position once it reaches the new position
+        if (Vector3.Distance(targetArea.GetComponent<RectTransform>().position, targetNewPos) <= 10f)
+        {
+            Vector3 newerPosition = new Vector3(0, UnityEngine.Random.Range(bottomArea.GetComponent<RectTransform>().position.y + 30, topArea.GetComponent<RectTransform>().position.y - 30));
+            targetNewPos = newerPosition;
+        }
+        Vector3.Lerp(targetArea.GetComponent<RectTransform>().position, targetNewPos, 5 * Time.deltaTime);
+    }
+
+    private void EndGame()
+    {
+        if (mainScript.failedCutting)
+            mainScript.scores[mainScript.currentBone - 1] = 100 - (mainScript.numberOfIncorrect * mainScript.pointsDamage) - mainScript.failedCuttingDamage;
+        else
+            mainScript.scores[mainScript.currentBone - 1] = 100 - (mainScript.numberOfIncorrect * mainScript.pointsDamage);
+        Mathf.Clamp(mainScript.scores[mainScript.currentBone - 1], 0, 100);
+        mainScript.numberOfIncorrect = 0;
+        mainScript.currentBone++;
+        startGame = false;
+        ResetValues();
     }
 }
--- Assets/Scripts/BoneMiniGameScript.cs
+++ Assets/Scripts/BoneMiniGameScript.cs
@@ -9,12 +9,20 @@ public class BoneMiniGameScript : MonoBehaviour
     [SerializeField] private int selectedBoneIndex = 0;
     private BoneCuttingMiniGameScript cuttingScript;
     [SerializeField] public int currentBone = 1; // Which bone in the order the player is at (starts at 1 ends at 6)
-    public bool startGame = false;
+    public bool start = false;
     private bool gameStarted = false;
     private float boneDamage = 10f; // how much damage the bone takes when selecting the wrong bone
 
     public LayerMask hitLayers; // Use organs layer
     private float maxDistance = Mathf.Infinity;
+
+    public List<float> scores = new List<float> { 0, 0, 0, 0, 0, 0 };
+    [SerializeField] private float currentScore = 100f;
+    [HideInInspector] public float pointsDamage = 10f;  // How many points the player loses when selecting the wrong bone
+    [HideInInspector] public int numberOfIncorrect = 0;
+    public float scoreAverage = 0f;  // Average of all the scores in the scores list
+    public bool failedCutting = false;  // Becomes true if the player runs out of time during the cutting
+    [HideInInspector] public float failedCuttingDamage = 30f; // multiplies each score that was failed with this number
 
     void Start()
     {
@@ -23,9 +31,10 @@ public class BoneMiniGameScript : MonoBehaviour
 
     void Update()
     {
-        if (startGame == true)
+        if (start == true)
         {
-            RandomizeBoneOrder();
+            StartGame();
+
         }
 
         // Raycast out from camera from mouse cursor to highlight bones
@@ -47,10 +56,15 @@ public class BoneMiniGameScript : MonoBehaviour
         {
             StartCuttingGame();
         }
+
+        // Endgame logic and scoring
+        if (currentBone >= 7)
+            EndGame();
     }
 
-    private void RandomizeBoneOrder()
+    private void StartGame()
     {
+        // Randomizes the order of ribs to cut
         for (int i = boneOrder.Count - 1; i > 0; i--)
         {
             int j = Random.Range(0, i + 1);
@@ -58,9 +72,16 @@ public class BoneMiniGameScript : MonoBehaviour
             boneOrder[i] = boneOrder[j];
             boneOrder[j] = temp;
         }
-        startGame = false;
+        start = false;
         selectedBoneIndex = 0;
         currentBone = 1;
+
+        // Resets scores
+        for (int i = 0; i < scores.Count; i++)
+        {
+            scores[i] = 0f;
+        }
+        scoreAverage = 0f;
     }
 
     private void SelectBone()
@@ -70,26 +91,30 @@ public class BoneMiniGameScript : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, maxDistance, hitLayers))
         {
-            /*int i;
-            for (i = boneObjects.Count - 1; i > 0; i--)
-            {
-                if (hit.collider.gameObject == boneObjects[i+1] && boneOrder[i+1] == currentBone)
-                {
-                    Debug.Log(boneOrder[i]);
-                    selectedBoneIndex = boneOrder[i];
-                    return;
-                }
-            }*/
             if (boneObjects.Contains(hit.collider.gameObject))
+            {
                 selectedBoneIndex = boneOrder[boneObjects.IndexOf(hit.collider.gameObject)];
-
+            }
             if (selectedBoneIndex != currentBone)
-                Debug.Log("Incorrect Bone");
+                numberOfIncorrect++;
         }
     }
 
     private void StartCuttingGame()
     {
+        failedCutting = false;
         cuttingScript.startGame = true;
+    }
+
+    private void EndGame()
+    {   
+        currentBone = 1;
+        // Handle scoring and value resets here
+        foreach (float score in scores)
+        {
+            scoreAverage = scoreAverage + score;
+            Debug.Log(scoreAverage);
+        }
+        scoreAverage = scoreAverage / 6;
     }
 }
