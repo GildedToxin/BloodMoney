4b9e02254a882acf36e0800af15c936a48eb6b0e
Necraza <doriangyanez@gmail.com>
2026-03-04 14:33:42 -0500

    Mouse movements tracked and model placement

---
+++ Assets/Scripts/HandFoot Minigame/MouseFollower.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class MouseFollower : MonoBehaviour
+{
+    private Vector3 position;
+    public float offset = 3f;
+
+    Ray ray;
+    RaycastHit hit;
+    private bool follow = false;
+
+    private void Update()
+    {
+        position = Input.mousePosition;
+        ray = Camera.main.ScreenPointToRay(position);
+        position.z = offset;
+
+        if(Physics.Raycast(ray, out hit) && hit.collider.name == "Mouse_Follower")
+        {
+            follow = true;
+        }
+
+        if (follow == true)
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(position);
+        }
+    }
+}
