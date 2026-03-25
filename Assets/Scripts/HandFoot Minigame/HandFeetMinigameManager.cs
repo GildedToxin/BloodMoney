9105abb71ee2a0199af02ed98aff16815fa2c716
Necraza <doriangyanez@gmail.com>
2026-03-25 03:02:19 -0400

    minigame Start code

--- Assets/Scripts/HandFoot Minigame/HandFeetMinigameManager.cs
+++ Assets/Scripts/HandFoot Minigame/HandFeetMinigameManager.cs
@@ -6,6 +6,7 @@ public class HandFeetMinigameManager : MonoBehaviour
 {
     public List<GameObject> limbs = new List<GameObject>();
     public List<GameObject> mazes = new List<GameObject>();
+    public List<GameObject> pointerHolder = new List<GameObject>();
     public int currentMaze = 0;
 
     private Vector3 position;
@@ -18,7 +19,16 @@ public class HandFeetMinigameManager : MonoBehaviour
     public GameObject raycastHolder;
 
     public float score = 100f;
-    public float pointDeduction = 0.2f; 
+    public float pointDeduction = 0.2f;
+
+    public void Start()
+    {
+        currentMaze = Random.Range(0, 3);
+        limbs[Random.Range(0, 3)].SetActive(true);
+        mazes[currentMaze].SetActive(true);
+
+        mouseFollower.transform.position = pointerHolder[currentMaze].transform.position;
+    }
 
     public void Update()
     {
