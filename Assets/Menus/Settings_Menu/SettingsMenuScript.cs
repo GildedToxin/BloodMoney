f86b8bf67fd3ae08f8c50bdc00015fd9c8be12ec
Necraza <doriangyanez@gmail.com>
2026-04-01 02:15:50 -0400

    Resolution, Full Screen, and Quality settings complete

--- Assets/Menus/Settings_Menu/SettingsMenuScript.cs
+++ Assets/Menus/Settings_Menu/SettingsMenuScript.cs
@@ -1,13 +1,61 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using NUnit.Framework;
+using System.Collections.Generic;
+using TMPro;
 
 public class SettingsMenuScript : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
 
     public void setVolume (float volume)
     {
         audioMixer.SetFloat("volume", volume);
+    }
+
+    public void setQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel (qualityIndex);
+    }
+
+    public void fullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void setResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
