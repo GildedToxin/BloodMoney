54c4b8b1f9815814dddf450ba32ee7ab349f120d
Toxin4426 <Toxin4426@gmail.com>
2026-04-03 23:39:24 -0400

    Fix organ bill board

--- Assets/Scripts/BillboardUI.cs
+++ Assets/Scripts/BillboardUI.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
 
 public class BillboardUI : MonoBehaviour
-{   void LateUpdate()
+
+{
+    public Transform target;
+    public float heightOffset = 2f;
+
+    void LateUpdate()
     {
+        // Always stay above the object's CENTER in world space
+        transform.position = target.position + Vector3.up * heightOffset;
+
+        transform.rotation = Quaternion.identity;
         transform.LookAt(Camera.main.transform);
         transform.Rotate(0, 180, 0); // Fix flipping if needed
     }
