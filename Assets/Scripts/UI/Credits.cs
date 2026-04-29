c19db3acd2af497452a898df3497328eec69e476
Necraza <doriangyanez@gmail.com>
2026-04-28 23:15:47 -0400

    Playtesters added to credits

--- Assets/Scripts/UI/Credits.cs
+++ Assets/Scripts/UI/Credits.cs
@@ -1,8 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Credits : MonoBehaviour
 {
+    public List<GameObject> credits = new List<GameObject>();
+    public int listPosition = 0;
+    public Button next;
+    public Button prev;
+
     public void backButton()
     {
         GameManager.Instance.PlayUIButtonPress();
@@ -11,5 +18,33 @@ public class Credits : MonoBehaviour
     public void Hover()
     {
         GameManager.Instance.ButtonHover();
+    }
+
+    public void nextButton()
+    {
+        listPosition++;
+        loadCredits();
+    }
+
+    public void previousButton()
+    {
+        listPosition--;
+        loadCredits();
+    }
+
+    public void loadCredits()
+    {
+        for (int i = 0; i < credits.Count; i++)
+        {
+            if (i == listPosition)
+            {
+                credits[i].SetActive(true);
+            }
+            else if (i != listPosition) 
+            {
+                credits[i].SetActive(false);
+                    
+            }
+        }
     }
 }
